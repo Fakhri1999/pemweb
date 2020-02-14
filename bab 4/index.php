ac5fcb0ea5d60a163e116614c1e5046bd9164bc5
rifkisrg <rfksrg@gmail.com>
2020-02-14 14:11:10 +0700

    init commit

---
+++ bab 4/index.php
@@ -0,0 +1,13 @@
+<?php
+$data = true;
+$data1 = "2";
+// if($data == true && $data == "1") { 
+//     if()
+//  echo
+// } else {
+// echo
+// }
+// echo $data == true ? $data1 == "1" ? 
+// "benar dan 1" : "salah dan 0" 
+// : "salah";
+
---
+++ bab 4/lat1.php
@@ -0,0 +1,9 @@
+<?php
+
+$var = 9;
+$var += 1;
+
+$var_2 = $var+1;
+
+echo $var . '<br>';
+echo $var_2;
